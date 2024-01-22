935f9b5a2d0483207c5899ff56d57bacec3a3e56
Guilherme Kila <958642@renner.onmicrosoft.com>
2024-01-22 12:56:27 -0300

    Project Struture

--- Screens/GoogleSearchScreen.cs
+++ Screens/GoogleSearchScreen.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace AutomationProject.Screens
 {
     public class GoogleSearchScreen
     {
-        
+        private  IWebDriver driver;
+         private  WebDriverWait wait;
+
+        public GoogleSearchScreen(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }        
+
+
+
     }
 }
